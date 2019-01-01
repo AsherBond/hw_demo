17da5ace607e07d274b5b7d8031838156ab0842e
Asher Bond <asher.bond@elasticprovisioner.com>
2019-01-01 14:13:01 -0800

    introduced basic regression test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash --posix
+# tests hw_demo app for basic regressions
+
+# show all steps
+set -x
+
+# exit if there are any errors
+set -e
+
+# requires egrep
+type egrep
+
+# "hello world output should be present. one comma is allowed after hello.
+# other output is allowed as long as output begins with case insensitive hello world
+./hw_demo | egrep -i "^hello[,]? world"
+
+echo "hw_demo basic regression test successful."
