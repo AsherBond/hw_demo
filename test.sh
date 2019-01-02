9306611e74dbff08e4e8e8688e445a9dbb19129a
Asher Bond <asher.bond@elasticprovisioner.com>
2019-01-02 03:10:26 -0800

    show output before test

--- test.sh
+++ test.sh
@@ -12,6 +12,7 @@ type egrep
 
 # "hello world output should be present. one comma is allowed after hello.
 # other output is allowed as long as output begins with case insensitive hello world
+./hw_demo
 ./hw_demo | egrep -i "^hello[,]? world"
 
 echo "hw_demo basic regression test successful."
